e39fdb232825f71264e93f6b17fb4228fe496f59
yomZsamora <samora.y@adzumi.co.ke>
2022-01-22 18:51:31 +0300

    Handle an event in a form based on clicking a submit button.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	let taskList = document.getElementById('tasks');
+	let taskEntry = document.getElementById('create-task-form');
+
+	taskEntry.onsubmit = (e) => {
+		e.preventDefault(); 
+		let newTaskDescription = document.getElementById('new-task-description').value;
+		var entry = document.createElement('li');
+		entry.appendChild(document.createTextNode(newTaskDescription));
+		taskList.appendChild(entry);
+		taskEntry.reset()
+	}
+
+	taskList.onclick = () => {
+
+	}
 });
